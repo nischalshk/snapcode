6fb16c34c8c6fd13a50d83609e0c81ca567e381d
Nischal Shakya <Nischal.shk@gmail.com>
2021-02-10 17:02:20 +0545

    experience list started

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,46 +1,55 @@
-import React, { Fragment, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { getCurrentProfile } from '../../actions/profile';
-import Spinner from '../common/Spinner';
-import DashboardActions from './DashboardActions'
- 
-const Dashboard=({getCurrentProfile,auth:{user},profile:{profile,loading}})=>{
-  useEffect(()=>{
-    getCurrentProfile()
+import React, { Fragment, useEffect } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getCurrentProfile } from "../../actions/profile";
+import Spinner from "../common/Spinner";
+import DashboardActions from "./DashboardActions";
+import Experience from "./Experience";
 
-
-
-  },[])
-    return loading && profile === null? <Spinner />:
-    
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading },
+}) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
     <Fragment>
       <h1 className="large text-primary">Dashboard</h1>
-      <p className='lead'>
-      <i className='fas fa-user'></i>
-      Welcome {user && user.name }
-</p>
-{profile !==null? (<Fragment>
-  
-  <DashboardActions/></Fragment>):(<Fragment>
-  <p>you have not setup a profile</p>
-<Link to='/create-profile' className="btn btn-primary my-1">Setup your Profile</Link>  
-  </Fragment>)}
-
-
+      <p className="lead">
+        <i className="fas fa-user"></i>
+        Welcome {user && user.name}
+      </p>
+      {profile !== null ? (
+        <Fragment>
+          <DashboardActions />
+          <Experience experience={profile.experience} />
+        </Fragment>
+      ) : (
+        <Fragment>
+          <p>you have not setup a profile</p>
+          <Link to="/create-profile" className="btn btn-primary my-1">
+            Setup your Profile
+          </Link>
+        </Fragment>
+      )}
     </Fragment>
+  );
 };
 
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
-  profile: PropTypes.object.isRequired
+  profile: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   profile: state.profile,
-  auth: state.auth
+  auth: state.auth,
 });
 
 export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
---
+++ client/src/components/dashboard/Education.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Education = ({ education }) => {
+  return <div></div>;
+};
+
+Education.propTypes = {
+  education: PropTypes.func.isRequired,
+};
+
+export default Education;
