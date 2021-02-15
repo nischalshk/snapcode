ba5f26cf18b606a1214ea547856acdcf446b8033
nischalshk <nischal.shk@gmail.com>
2021-02-15 08:41:45 +0545

    Posts mapped

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,14 +1,30 @@
-import React, { Fragment, useEffect } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { getPosts } from "../../actions/post";
-import Spinner from "../common/Spinner";
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getPosts } from '../../actions/post';
+import Spinner from '../common/Spinner';
+import PostItem from './PostItem';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return <div></div>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i>Welcome to the community
+      </p>
+
+      <div className='posts'>
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
