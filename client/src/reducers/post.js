c2013aa47928064d19af4ef3ed72475a600d3c98
nischalshk <nischal.shk@gmail.com>
2021-02-15 09:29:02 +0545

    working on post form

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,10 @@
-import { GET_POSTS, POST_ERROR } from "../actions/types";
+import {
+  DELETE_POST,
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  ADD_POST,
+} from '../actions/types';
 
 const initialState = {
   posts: [],
@@ -17,6 +23,18 @@ export default function (state = initialState, action) {
         posts: payload,
         loading: false,
       };
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [...state.posts, payload],
+        loading: false,
+      };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter((post) => post._id !== payload),
+        loading: false,
+      };
 
     case POST_ERROR:
       return {
@@ -24,6 +42,15 @@ export default function (state = initialState, action) {
         error: payload,
         loading: false,
       };
+    case UPDATE_LIKES:
+      return {
+        ...state,
+        posts: state.posts.map((post) =>
+          post._id === payload.id ? { ...post, likes: payload.likes } : post
+        ),
+        loading: false,
+      };
+
     default:
       return state;
   }
